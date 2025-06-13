b0794a0156f3d1c73411e6163d24d716f6403e60
ChahineM <chahine38@hotmail.fr>
2025-06-13 13:30:46 +0200

    Update hours_bypass_connexion.ps1

--- Ressources/hours_bypass_connexion.ps1
+++ Ressources/hours_bypass_connexion.ps1
@@ -1,4 +1,4 @@
-Autoriser 7h à 18h (heure locale), du lundi au vendredi
+Autoriser 7h à 18h (heure locale), du lundi au vendredi, bypass cette règle pour les administrateurs
 $users = Get-ADUser -Filter * -SearchBase "OU=LabUtilisateurs,DC=ekoloclast,DC=local"
 foreach ($user in $users) {
     if (-not (Get-ADGroupMember "Groupe_Bypass_Connexion" | Where-Object {$_.distinguishedName -eq $user.DistinguishedName})) {
